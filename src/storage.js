42122ba795c0dbdc15200468eb80c2df0a25b058
githubchief <ranganathanshams@gmail.com>
2023-01-22 19:35:15 +0530

    add console and get project title in getLoP

--- src/storage.js
+++ src/storage.js
@@ -4,19 +4,19 @@ import { task } from "./task";
 
 const storage = () => {
     
-    localStorage.setItem('lop',JSON.stringify(""));
+    //initialisation
+    //localStorage.setItem('lop',JSON.stringify(" "));
+
     const saveListOfProjects = (data) => {
         localStorage.setItem('lop', JSON.stringify(data));
         console.log(JSON.parse(localStorage.getItem('lop')));
     }
 
     const getListOfProjects = () => {
-        let lop = Object.assign(listOfProjects(), JSON.parse(localStorage.getItem('lop'))) ;
-
-        lop.setProject( lop.getProjects().map((prj) => Object.assign(project, prj)) );
-
+        let lop = Object.assign(listOfProjects, JSON.parse(localStorage.getItem('lop'))) ;
+        console.log(lop);
+        lop.setProject( lop.getProjects().map((prj) => prj.getProjectTitle()));
         lop.getProjects().forEach((prj => prj.setProjectTasks(prj.getProjectTasks().map((tempTask)=> Object.assign(task, tempTask)))));
-
         return lop;
     }
 
