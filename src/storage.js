097967c806bae94b17cfdb2884fcdcf8c030dc35
githubchief <ranganathanshams@gmail.com>
2023-01-24 11:51:48 +0530

    try initialising ProjectList in storage

--- src/storage.js
+++ src/storage.js
@@ -3,17 +3,17 @@ import { project } from "./project";
 import { task } from "./task";
 
 const storage = () => {
-    
-    //localStorage.setItem('lop',JSON.stringify(""));
-    
+    let lop = listOfProjects();
+    localStorage.setItem('lop',JSON.stringify(lop.projectList));
+    console.log(lop.projectList);
     const saveListOfProjects = (data) => {
         localStorage.setItem('lop', JSON.stringify(data));
         console.log(JSON.parse(localStorage.getItem('lop')));
     }
 
     const getListOfProjects = () => {
-        let lop = Object.assign(listOfProjects, JSON.parse(localStorage.getItem('lop'))) ;
-        console.log(lop);
+        let lop = Object.assign(listOfProjects.projectList, JSON.parse(localStorage.getItem('lop'))) ;
+        console.log(JSON.parse(localStorage.getItem('lop')));
         lop.setProject( lop.getProjects().map((prj) => prj.getProjectTitle()));
         lop.getProjects().forEach((prj => prj.setProjectTasks(prj.getProjectTasks().map((tempTask)=> Object.assign(task, tempTask)))));
         return lop;
